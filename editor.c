74be25c7d59250ce6515edbbde43e7b3f04b66f8
Sambhav Jain <sambhavjain2612@gmail.com>
2018-02-18 23:33:47 +0530

    step 20 update

--- editor.c
+++ editor.c
@@ -8,31 +8,49 @@
 #include <errno.h>
 
 // modular headers :wink:
-#include "app/rawMode.h"
+#include "sub-headers/rawMode.h"
+
+#define CTRL_KEY(k) ((k)&0x1f)
+
+void editorRefreshScreen()
+{
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+}
+
+char editorReadKey()
+{
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1)
+    {
+        if (nread == -1 && errno != EAGAIN)
+            die("read");
+    }
+    return c;
+}
+
+void editorProcessKeypress()
+{
+    char c = editorReadKey();
+    // printf("%d\r\n", c); => works on exit
+    switch (c)
+    {
+    case CTRL_KEY('q'):
+        exit(0);
+        break;
+
+    }
+
+}
 
 // driver functions
 int main()
 {
-
     switchToRawMode();
-
     while (1)
     {
-        char ch = '\0';
-        if (read(STDIN_FILENO, &ch, 1) == -1 && errno != EAGAIN)
-            die("read");
-
-        if (iscntrl(ch))
-        {
-            printf("%d\r\n", ch);
-        }
-        else
-        {
-            printf("%d ('%c')\r\n", ch, ch);
-        }
-        if (ch == 'q')
-            break;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
-
     return 0;
 }

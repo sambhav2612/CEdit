a694f7294b19c72427466ef3f7bcca7f1d1e2835
sambhav2612 <sambhavjain2612@gmail.com>
2018-02-19 15:17:43 +0530

    build failing

--- sub-headers/refreshScreen.h
+++ sub-headers/refreshScreen.h
@@ -13,7 +13,7 @@ void editorRefreshScreen()
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
     
     abAppend(&ab, buf, strlen(buf));
-    abAppend(ab, "\x1b[25h", 6);
+    abAppend(ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);

7b7f6f63c2942be62ef2a1eb48161616e38c1c23
Jeremy Ruten <jeremy.ruten@gmail.com>
2018-02-19 09:16:44 -0600

    Fix build

--- output/refreshScreen.h
+++ output/refreshScreen.h
@@ -2,18 +2,18 @@ void editorRefreshScreen()
 {
     struct abuf ab = ABUF_INIT;
 
-    abAppend(ab, "\x1b[?25l", 6);
+    abAppend(&ab, "\x1b[?25l", 6);
     // abAppend(ab, "\x1b[2J", 4);
-    abAppend(ab, "\x1b[H", 3);
+    abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
 
     char buf[32];
-    
+
     snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
-    
+
     abAppend(&ab, buf, strlen(buf));
-    abAppend(ab, "\x1b[?25h", 6);
+    abAppend(&ab, "\x1b[?25h", 6);
 
     write(STDOUT_FILENO, ab.b, ab.len);
     abFree(&ab);

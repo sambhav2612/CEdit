4dc99dd358de06f64704a8ab3510609c1b3892f9
sambhav2612 <sambhavjain2612@gmail.com>
2018-03-01 21:32:48 +0530

    add keymap support

--- terminal/readKeypress.h
+++ terminal/readKeypress.h
@@ -7,5 +7,25 @@ char editorReadKey()
         if (nread == -1 && errno != EAGAIN)
             die("read");
     }
+
+    id (c == '\x1b') {
+        char seq[3];
+
+        if (read(STDIN_FILENO, &seq[0], 1) != 1)
+            return '\x1b';
+
+        if (read(STDIN_FILENO, &seq[1], 1) != 1)
+            return '\x1b';       
+
+        if (seq[0] == '[') {
+            switch (seq[0]) {
+                case 'A': return 'w';
+                case 'B': return 'a';
+                case 'C': return 's';
+                case 'D': return 'd';
+            }
+        }
+    }
+
     return c;
 }

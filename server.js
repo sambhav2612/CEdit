14d50bccc0e1b82988e8639e671164cb3b9ebbb0
sambhav2612 <sambhavjain2612@gmail.com>
2018-03-01 21:18:11 +0530

    refactor server.js code

--- server.js
+++ server.js
@@ -7,24 +7,25 @@ if (process.argv.length <= 2) {
 
 } else {
 	var app = express();
-	var server = app.listen(3000);
+	var port = process.env.PORT || 3000;
 
-	if (server) {
-		console.log('Server istening on port 3000');
-	}
+	app.use(require('body-parser').urlencoded({ extended: true }));
+	app.use(require('body-parser').json());
 
 	var path = process.argv[2];
 	var items;
 
 	fs.readdir(path, function (err, content) {
 		items = content;
+		//console.log(items);
 	});
 
-	app.use(express.static('/'));
+	app.get('/', function (req, res) {
+		//console.log(req);
+		res.json({ "response": items });
+	});
 
-	app.get('/', print);
-
-	function print(request, response) {
-		response.send(items);
+	if (app.listen(port)) {
+		console.log('Server istening on port ' + port);
 	}
 }

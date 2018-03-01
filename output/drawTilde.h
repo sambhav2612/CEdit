0b8493dca9afd5d2c8859b08077b8eeefa49a583
sambhav2612 <sambhavjain2612@gmail.com>
2018-03-01 23:27:30 +0530

    update erow

--- output/drawTilde.h
+++ output/drawTilde.h
@@ -46,11 +46,14 @@ void editorDrawRows(struct abuf *ab)
 }
 
 void editorAppendRow(char *s, size_t len) {
-  E.row->size = len;
-  E.row->chars = malloc(len + 1);
+  E.row = realloc(E.row, sizeof(erow) * (E.numrows+1));
   
-  memcpy(E.row->chars, s, len);
+  int at = E.numrows;
+  E.row[at].size = len;
+  E.row[at].chars = malloc(len + 1);
   
-  E.row->chars[len] = '\0';
-  E.numrows = 1;
+  memcpy(E.row[at].chars, s, len);
+  
+  E.row[at].chars[len] = '\0';
+  E.numrows++;
 }

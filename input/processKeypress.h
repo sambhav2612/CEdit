c4f787b898d493e64f2ac510b498ebb39fae5218
sambhav2612 <sambhavjain2612@gmail.com>
2018-03-30 10:28:42 +0530

    page down working

--- input/processKeypress.h
+++ input/processKeypress.h
@@ -1,49 +1,45 @@
 void editorMoveCursor(int key) {
-    erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
-    
-    switch(key) {
-        case ARROW_LEFT:
-            if (E.cx != 0) {
-                E.cx--;
-            } else if (E.cy > 0) {
-                E.cy--;
-                E.cx = E.row[E.cy].size;
-            }
-            break;
-        
-        case ARROW_RIGHT:
-            if (row && E.cx < row->size) {
-                E.cx++;
-            } else if (row && E.cx == row->size) {
-                E.cy++;
-                E.cx = 0;
-            }
-            break;
-        
-        case ARROW_UP:
-            if (E.cy != 0) { 
-                E.cy--;
-            }
-            break;
-    
-        case ARROW_DOWN:
-            if (E.cy != E.numrows) { 
-                E.cy++;
-            }
-            break;
-    }
+  erow *row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
 
-    row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
-    int rowlen = row ? row->size : 0;
-    if (E.cx > rowlen) {
-        E.cx = rowlen;
-    }
+  switch (key) {
+    case ARROW_LEFT:
+      if (E.cx != 0) {
+        E.cx--;
+      } else if (E.cy > 0) {
+        E.cy--;
+        E.cx = E.row[E.cy].size;
+      }
+      break;
+    case ARROW_RIGHT:
+      if (row && E.cx < row->size) {
+        E.cx++;
+      } else if (row && E.cx == row->size) {
+        E.cy++;
+        E.cx = 0;
+      }
+      break;
+    case ARROW_UP:
+      if (E.cy != 0) {
+        E.cy--;
+      }
+      break;
+    case ARROW_DOWN:
+      if (E.cy < E.numrows) {
+        E.cy++;
+      }
+      break;
+  }
+
+  row = (E.cy >= E.numrows) ? NULL : &E.row[E.cy];
+  int rowlen = row ? row->size : 0;
+  if (E.cx > rowlen) {
+    E.cx = rowlen;
+  }
 }
 
 void editorProcessKeypress()
 {
     int c = editorReadKey();
-    // printf("%d\r\n", c); => works on exit
     switch (c)
     {
     case CTRL_KEY('q'):
@@ -69,7 +65,7 @@ void editorProcessKeypress()
             if (c == PAGE_UP) {
                 E.cy = E.rowoff;
             } else if (c == PAGE_DOWN) {
-                E.cy = E.rowoff + E.screenColumns - 1;
+                E.cy = E.rowoff + E.screenRows - 1;
                 if (E.cy > E.numrows) {
                     E.cy = E.numrows;
                 }

85d6c987b69a4b8fc60ac3eccc6a372552c34c25
Sambhav Jain <sambhavjain2612@gmail.com>
2018-06-07 20:48:03 +0530

    add docusaurus
    Signed-off-by: Sambhav Jain <sambhavjain2612@gmail.com>

--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-cc editor.c -o editor
+
+sudo 
+
+dpkg -s $1 $2 &> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Package  is installed!"
+else
+    echo "Package  is NOT installed!"
+fi
+
+gcc editor.c -o editor
 make
